6e0903ecde7ccfd320422abf9b95b4511fe243d0
Kim Vu <kimhuongvu1801@gmail.com>
2025-12-26 10:12:12 +0000

    stg_bike model created

--- models/trip_fact.sql
+++ models/trip_fact.sql
@@ -7,7 +7,7 @@ with trips as (
         end_station_id,
         member_casual,
         timestampdiff(second, to_timestamp(started_at), to_timestamp(ended_at)) as trip_duration_seconds
-    from {{ source('demo', 'bike') }}
+    from {{ ref('stg_bike') }}
 )
 select 
 *
